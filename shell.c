0c4e3bf17d4df843761da711dd83bb84b38a4c79
Juan <juan.verao@udea.edu.co>
2024-10-27 21:42:59 -0500

    comandos exit y cd añadidos

--- shell.c
+++ shell.c
@@ -2,111 +2,108 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <sys/types.h>
-#include <errno.h>
+#include <sys/wait.h>
 
-#define MAX_INPUT_SIZE 1024
-#define MAX_ARGS 64
-#define MAX_PATHS 64
+#define MAX_PATHS 10
+#define MAX_ARGS 100
 
-char *path[MAX_PATHS] = { "/bin", NULL };  // Inicializa la ruta con /bin
+char *path[MAX_PATHS]; // Inicializa el array de paths
+int path_count = 0;     // Cuenta de los directorios en el path
+char error_message[] = "An error has occurred\n";
 
-void print_exec_error(char *command) {
-    fprintf(stderr, "execv failed for %s: %s\n", command, strerror(errno));
-}
-
+// Función para ejecutar comandos externos
 void execute_command(char **args) {
-    pid_t pid = fork();
-    
-    if (pid < 0) {
-        // Error al hacer fork
-        perror("fork failed");
-        exit(1);
-    } else if (pid == 0) {
-        // Proceso hijo: ejecuta el comando
-        char executable_path[MAX_INPUT_SIZE];
-        int found = 0;
-        
-        for (int i = 0; path[i] != NULL; i++) {
-            snprintf(executable_path, sizeof(executable_path), "%s/%s", path[i], args[0]);
-            printf("Trying path: %s\n", executable_path);  // Para depuración
-            if (access(executable_path, X_OK) == 0) {
-                found = 1;
-                execv(executable_path, args);
-                print_exec_error(executable_path);  // En caso de error
-                exit(1);
+    for (int i = 0; i < path_count; i++) {
+        char exec_path[1024];
+        snprintf(exec_path, sizeof(exec_path), "%s/%s", path[i], args[0]);
+
+        if (access(exec_path, X_OK) == 0) {
+            if (fork() == 0) {
+                execv(exec_path, args);
+                exit(0);
+            } else {
+                wait(NULL);
             }
+            return;
         }
-        
-        if (!found) {
-            fprintf(stderr, "An error has occurred\n");
-        }
-        exit(1);
-    } else {
-        // Proceso padre: espera a que el hijo termine
-        wait(NULL);
     }
+    write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
-void parse_and_execute(char *input) {
-    char *args[MAX_ARGS];
-    int i = 0;
-    
-    // Parsing de la línea de entrada
-    char *token = strsep(&input, " \t\n");
-    while (token != NULL && i < MAX_ARGS - 1) {
-        args[i++] = token;
-        token = strsep(&input, " \t\n");
-    }
-    args[i] = NULL;  // Termina la lista de argumentos con NULL
+// Función principal del shell
+void wish_shell() {
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
 
-    // Ejecuta el comando si se ha ingresado alguno
-    if (args[0] != NULL) {
+    // Inicializa el path con "/bin"
+    path[0] = strdup("/bin");
+    path_count = 1;
+
+    while (1) {
+        printf("wish> ");
+        read = getline(&line, &len, stdin);
+        if (read == -1) {
+            break;
+        }
+
+        // Tokenizar la línea de entrada
+        char *args[MAX_ARGS];
+        int arg_count = 0;
+        char *token = strtok(line, " \t\n");
+        while (token != NULL && arg_count < MAX_ARGS - 1) {
+            args[arg_count++] = token;
+            token = strtok(NULL, " \t\n");
+        }
+        args[arg_count] = NULL;
+
+        if (arg_count == 0) continue;
+
+        if (strcmp(args[0], "exit") == 0) {
+            if (args[1] != NULL) {
+                write(STDERR_FILENO, error_message, strlen(error_message));
+            } else {
+                free(line);
+                for (int i = 0; i < path_count; i++) {
+                    free(path[i]);
+                }
+                exit(0);
+            }
+            continue;
+        }
+
+        if (strcmp(args[0], "cd") == 0) {
+            if (args[1] == NULL || args[2] != NULL) {
+                write(STDERR_FILENO, error_message, strlen(error_message));
+            } else {
+                if (chdir(args[1]) != 0) {
+                    write(STDERR_FILENO, error_message, strlen(error_message));
+                }
+            }
+            continue;
+        }
+
+        if (strcmp(args[0], "path") == 0) {
+            for (int i = 0; i < path_count; i++) {
+                free(path[i]);
+            }
+            path_count = 0;
+
+            for (int i = 1; args[i] != NULL && path_count < MAX_PATHS; i++) {
+                path[path_count] = strdup(args[i]);
+                path_count++;
+            }
+            continue;
+        }
+
         execute_command(args);
     }
+
+    free(line);
 }
 
 int main(int argc, char *argv[]) {
-    FILE *input_stream;
-
-    // Modo batch
-    if (argc == 2) {
-        input_stream = fopen(argv[1], "r");
-        if (!input_stream) {
-            perror("fopen failed");
-            exit(1);
-        }
-    }
-    // Modo interactivo
-    else if (argc == 1) {
-        input_stream = stdin;
-    } else {
-        fprintf(stderr, "An error has occurred\n");
-        exit(1);
-    }
-
-    char input[MAX_INPUT_SIZE];
-    
-    // Loop principal del shell
-    while (1) {
-        if (input_stream == stdin) {
-            printf("wish> ");
-        }
-        
-        // Lee una línea de entrada
-        if (fgets(input, MAX_INPUT_SIZE, input_stream) == NULL) {
-            break;  // EOF
-        }
-        
-        // Procesa y ejecuta el comando ingresado
-        parse_and_execute(input);
-    }
-
-    // Cierra el archivo si está en modo batch
-    if (input_stream != stdin) {
-        fclose(input_stream);
-    }
-
+    wish_shell();
     return 0;
 }
